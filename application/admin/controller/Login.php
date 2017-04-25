80632b0d17698e398354eaae0540dd7a8410f9df
zhuxinbiao <46100702@qq.com>
2017-04-25 11:51:27 +0800

    新增学生登陆

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -94,4 +94,51 @@ class Login extends Controller
 
         $this->redirect(url('index'));
     }
+
+    //学生身份登陆
+    public function loginFree()
+    {
+        $username = "学生1";
+        $password = "123456";
+        $result = $this->validate(compact('username', 'password'), 'AdminValidate');
+        if(true !== $result){
+            return json(['code' => -5, 'data' => '', 'msg' => $result]);
+        }
+
+        $hasUser = db('admin')->where('username', $username)->find();
+        if(empty($hasUser)){
+            return json(['code' => -1, 'data' => '', 'msg' => '管理员不存在']);
+        }
+
+        if(md5(md5($password) . config('auth_key')) != $hasUser['password']){
+            writelog($hasUser['id'],$username,'用户【'.$username.'】登录失败：密码错误',2);
+            return json(['code' => -2, 'data' => '', 'msg' => '密码错误']);
+        }
+
+        if(1 != $hasUser['status']){
+            writelog($hasUser['id'],$username,'用户【'.$username.'】登录失败：该账号被禁用',2);
+            return json(['code' => -6, 'data' => '', 'msg' => '该账号被禁用']);
+        }
+
+        //获取该管理员的角色信息
+        $user = new UserType();
+        $info = $user->getRoleInfo($hasUser['groupid']);
+
+        session('username', $username);
+        session('uid', $hasUser['id']);
+        session('rolename', $info['title']);  //角色名
+        session('rule', $info['rules']);  //角色节点
+        session('name', $info['name']);  //角色权限
+
+        //更新管理员状态
+        $param = [
+            'loginnum' => $hasUser['loginnum'] + 1,
+            'last_login_ip' => request()->ip(),
+            'last_login_time' => time()
+        ];
+
+        Db::name('admin')->where('id', $hasUser['id'])->update($param);
+        writelog($hasUser['id'],session('username'),'用户【'.session('username').'】登录成功',1);
+        return json(['code' => 1, 'data' => url('index/index'), 'msg' => '登录成功']);
+    }
 }
